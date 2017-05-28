4d8875231b8c9b7ad077e1007f56f833158974c4
Milad Rastian <milad@high-mobility.com>
2017-05-28 13:07:58 +0200

    Route all the requests to elm

--- apps/gitwerk_web/lib/gitwerk_web/router.ex
+++ apps/gitwerk_web/lib/gitwerk_web/router.ex
@@ -13,14 +13,13 @@ defmodule Gitwerk.Web.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api/v1", Gitwerk.Web do
+    pipe_through :api
+  end
+
   scope "/", Gitwerk.Web do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Gitwerk.Web do
-  #   pipe_through :api
-  # end
 end
