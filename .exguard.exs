b112252ac9bb86baebd41098fe6633632ec6e5bb
Milad Rastian <eslashmili@gmail.com>
2017-09-02 16:07:35 +0200

    watch elm files and run elm-test on them

--- .exguard.exs
+++ .exguard.exs
@@ -1,7 +1,13 @@
 use ExGuard.Config
 
-guard("unit-test", run_on_start: true)
+guard("elixir test", run_on_start: true)
 |> command("mix test --color")
 |> watch(~r{\.(erl|ex|exs|eex|xrl|yrl)\z}i)
 |> ignore(~r{deps})
 |> notification(:off)
+
+guard("elm test", run_on_start: true)
+|> command("elm-test assets/tests/")
+|> watch(~r{\.(elm)\z}i)
+|> ignore(~r{elm-stuff})
+|> notification(:on)
