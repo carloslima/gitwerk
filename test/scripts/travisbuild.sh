598682332a5c57df21a3aa63f09080bd6f7484ef
Milad Rastian <eslashmili@gmail.com>
2017-10-22 19:36:45 +0200

    Don't prompt for passphrase

--- test/scripts/travisbuild.sh
+++ test/scripts/travisbuild.sh
@@ -2,9 +2,9 @@
 
 mkdir -p priv/test/ssh_keys
 cd priv/test/ssh_keys
-ssh-keygen -b 256 -t ecdsa -f ssh_host_ecdsa_key
-ssh-keygen -b 1024 -t dsa -f ssh_host_dsa_key
-ssh-keygen -b 2048 -t rsa -f ssh_host_rsa_key
+ssh-keygen -N '' -b 256 -t ecdsa -f ssh_host_ecdsa_key
+ssh-keygen -N '' -b 1024 -t dsa -f ssh_host_dsa_key
+ssh-keygen -N '' -b 2048 -t rsa -f ssh_host_rsa_key
 
 cd ~
 
